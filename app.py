05e84966c83e7be1a875bebe95381a709cbafbee
Raman <rmnchopra91@gmail.com>
2024-02-25 10:03:40 +0530

    adding code for user conversation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from functions import start_conversation
+from flask import Flask, render_template, redirect, url_for, request
+from functions import start_conversation, get_chat_model_completions, intent_confirmation_layer, extract_dictionary_from_string, compare_laptop_with_user_req, recommendation_validation, initialize_conversation_for_product_recommendation
 
 app = Flask(__name__)
 
@@ -13,6 +13,42 @@ print("Hello world -----")
 def default_func():
     global conversation_bot, conversation, top_3_laptops
     return render_template("index.html", conversation_list = conversation_bot)
+
+@app.route("/converse", methods = ['POST'])
+def converse():
+    global conversation_bot, conversation, top_3_laptops, conversation_reco
+    user_input = request.form["user_input"]
+
+    if top_3_laptops is None:
+        prompt = 'Remember your system message and that you are an intelligent laptop assistant. So, you only help with questions around laptop.'
+        content = user_input + prompt
+        conversation_bot.append({'user': user_input})
+
+        response_assistant = get_chat_model_completions(content)
+
+        confirmation = intent_confirmation_layer(response_assistant)
+
+
+        print("Response Assistant : ", response_assistant)
+        if "No" in confirmation:
+            conversation_bot.append({'bot':response_assistant})
+        else:
+            response = extract_dictionary_from_string(response_assistant)
+
+            conversation_bot.append({'bot':"Thank you for providing all the information. Kindly wait, while I fetch the products: \n"})
+            print("user requirement :::: ", response)
+            top_3_laptops = compare_laptop_with_user_req(response)
+            print("top_3_laptops ---- ", top_3_laptops)
+
+            validated_reco = recommendation_validation(top_3_laptops)
+
+            if len(validated_reco) == 0:
+                conversation_bot.append({'bot':"Sorry, we do not have laptops that match your requirements. Connecting you to a human expert. Please end this conversation."})
+
+            conversation_reco = initialize_conversation_for_product_recommendation(validated_reco)
+            conversation_bot.append({'bot':conversation_reco})
+    print("redirect to home page -------")
+    return redirect(url_for('default_func'))
 
 if __name__ == '__main__':
     app.run(debug=True, host= "0.0.0.0", port="5000")
--- functions.py
+++ functions.py
@@ -10,6 +10,9 @@ genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history = [])
 
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history = [])
+    
 def get_system_message():
     delimiter = "####"
     example_user_req = {'GPU intensity': 'high','Display quality': 'high','Portability': 'low','Multitasking': 'high','Processing speed': 'high','Budget': '150000'}
@@ -79,3 +82,136 @@ def start_conversation():
     response = chat.send_message(system_message)
     print("response : ", response.text)
     return response.text
+
+
+def get_chat_model_completions(message):
+    response = chat.send_message(message).text
+    return response
+
+def execute_prompt(prompt, model=model):
+  response = model.generate_content(prompt)
+  print(f"model.generate_content output --> {response}")
+  return response.text
+
+def intent_confirmation_layer(response_assistant):
+    delimiter = "####"
+    prompt = f"""
+    You are a senior evaluator who has an eye for detail.
+    You are provided an input. You need to evaluate if the input has the following keys: 'GPU intensity','Display quality','Portability','Multitasking',' Processing speed','Budget'
+    Next you need to evaluate if the keys have the the values filled correctly.
+    The values for all keys, except 'budget', should be 'low', 'medium', or 'high' based on the importance as stated by user. The value for the key 'budget' needs to contain a number with currency.
+    Output a string 'Yes' if the input contains the dictionary with the values correctly filled for all keys.
+    Otherwise out the string 'No'.
+    Here are some input output pairs for better understanding:
+    input: {{'GPU intensity': 'low', 'Display quality': 'high', 'Portability': 'low', 'Multitasking': 'high', 'Processing speed': 'medium', 'Budget': '50000'}}
+    output: Yes
+    input: {{'Display quality': 'high', 'Portability': 'low', 'Multitasking': 'high', 'Processing speed': 'medium', 'Budget': '50000'}}
+    output: No
+    input: {{'Portability': 'low', 'Multitasking': 'high', 'Processing speed': 'medium', 'Budget': '50000'}}
+    output: No
+    input: {{'Multitasking': 'high', 'Processing speed': 'medium', 'Budget': '50000'}}
+    output: No
+    input: {{'Budget': '50000'}}
+    output: No
+
+    Here is the input: {response_assistant}
+    Only output a one-word string - Yes/No.
+    """
+    return execute_prompt(prompt, model)
+
+def extract_dictionary_from_string(string: str) -> dict:
+  # Use regular expression to extract exact dictionary part
+  print("Extract Dictionary --- ", string)
+  regex_pattern = r"\{[^{}]+\}"
+  dictionary_matches = re.findall(regex_pattern, string)
+  dictionary = {}
+
+  try:
+    if dictionary_matches:
+        dictionary_match = dictionary_matches[0]
+        dictionary_string = dictionary_match.lower()
+
+        # Convert dictionary string into a python dictionary
+        dictionary = ast.literal_eval(dictionary_string)
+        print("dictionary --- ", dictionary)
+  except:
+     dictionary = {}
+
+  return dictionary
+
+def compare_laptop_with_user_req(user_req):
+  user_budget = user_req.get("budget", 0)
+  if type(user_budget) != int:
+    user_budget = user_budget.replace(",", "").split(" ")[0]
+    user_budget = int(user_budget)
+  print("user_req : ", user_req)
+  print("user_budget : ", user_budget)
+
+  # Load laptop dataframe
+  laptop_df= pd.read_csv('updated_laptop.csv')
+  filtered_laptops = laptop_df.copy()
+
+  # Filter laptops whose price is less than or equal to user_budget
+  filtered_laptops['Price'] = filtered_laptops['Price'].str.replace(",", "").astype(int)
+  filtered_laptops = filtered_laptops[filtered_laptops.Price <= user_budget].copy()
+  print("filtered_laptops ---- ", filtered_laptops)
+  mappings = {'low': 0, 'medium': 1, 'high': 2}
+
+  # Create Score column in dataframe and initialize it to 0
+  filtered_laptops['Score'] = 0
+  # Calculate score based on user_req
+  for index, row in filtered_laptops.iterrows():
+    user_product_match_str = row['laptop_feature']
+    laptop_values = extract_dictionary_from_string(user_product_match_str)
+    print("laptop_values : ", laptop_values)
+    score = 0
+
+    for key, user_value in user_req.items():
+      if key == 'budget':
+        continue # Skip budget comparision, its buisness requirement
+
+      laptop_value = laptop_values.get(key, None)
+      print(f"key : {key}, laptop_value : {laptop_value}")
+
+      laptop_mappings = mappings.get(laptop_value, -1)
+      user_mappings = mappings.get(user_value, -1)
+
+      # If the laptop value is greater than or equal to the user value then increment score by 1
+      if laptop_mappings >= user_mappings:
+        score += 1
+
+    filtered_laptops.loc[index, 'Score'] = score
+
+  # Sort the laptops by score in descending order and return top 3 products
+  top_laptops = filtered_laptops.drop('laptop_feature', axis=1)
+  top_laptops = top_laptops.sort_values('Score', ascending=False).head(3)
+  print("top_laptops :", top_laptops)
+
+  return top_laptops.to_json(orient='records')
+
+
+
+def recommendation_validation(laptop_recommendation):
+    data = json.loads(laptop_recommendation)
+    data1 = []
+    for i in range(len(data)):
+        if data[i]['Score'] > 2:
+            data1.append(data[i])
+
+    return data1
+
+
+def initialize_conversation_for_product_recommendation(products):
+  global chat
+  chat = model.start_chat(history = [])
+  system_message = f"""
+    You are an intelligent laptop gadget expert and you are tasked with the objective to solve the user \
+    queries about any product from the catalogue: {products}. \
+    You should keep the user profile in mind while answering the questions.\
+
+    Start with a brief summary of each laptop in the following format, in decreasing order of price of laptops:
+    1. <Laptop Name>: <Majof specification of laptop>, <Price in Rs>
+    2. <Laptop Name>: <Majof specification of laptop>, <Price in Rs>
+  """
+  response = chat.send_message(system_message)
+  return response.text
